a88e40e40f95c4404bee9b07bebff5ed15cf6028
Artyom <r.tem13@gmail.com>
2020-08-27 21:29:39 +0700

    Сделал черновик первой карточки

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-wrapper">
+        <div className="header">
+            <h1>Ты сегодня покормил кота?</h1>
+        </div>
+
+        <div className="wrapper">
+            <div className="item item-first">
+                <div className="item-header-first">
+                    <h3>Сказочное заморское яство</h3>
+                    <h1>Нямушка</h1>
+                    <h2>с фуа-гра</h2>
+                    <ul>
+                        <li>10 порций</li>
+                        <li>1 мышь в подарок</li>
+                    </ul>
+                </div>
+                <div className="circle">
+                    <ul className="weight-list">
+                        <li className="weight"><strong>0.5</strong></li>
+                        <li className="kg"><strong>кг</strong></li>
+                    </ul>
+                </div>
+                <img src="../img/cat.jpg" alt="кот"/>
+
+            </div>
+
+            <div className="item">2</div>
+            <div className="item">3</div>
+        </div>
+
     </div>
   );
 }
