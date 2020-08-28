2550b99aa1a0887eec8f3a8ecdecbb71ac7be39d
Диспетчер <C:\Users\disp\AppData\Roaming\The Bat!>
2020-08-28 21:19:31 +0700

    переработал блок

--- src/App.js
+++ src/App.js
@@ -9,28 +9,51 @@ function App() {
         </div>
 
         <div className="wrapper">
-            <div className="item item-first">
-                <div className="item-header-first">
-                    <h3>Сказочное заморское яство</h3>
-                    <h1>Нямушка</h1>
-                    <h2>с фуа-гра</h2>
-                    <ul>
-                        <li>10 порций</li>
-                        <li>1 мышь в подарок</li>
-                    </ul>
+
+            <div className="item">
+                
+                <div className="item__block">                    
+                    <div className="item__top">
+                        <div className="item__corner"></div>
+                        <div className="item__parag">
+                            <span className="item__description">Сказочное заморское яство</span>
+                        </div>
+                    </div>
+                    <div className="item__header">
+                        
+                        <h1>Нямушка</h1>
+                        <h2>с фуа-гра</h2>
+                        <ul>
+                            <li>10 порций</li>
+                            <li>1 мышь в подарок</li>
+                        </ul>
+                    </div>
+                    <div className="circle">
+                        <ul className="weight-list">
+                            <li className="weight"><strong>0.5</strong></li>
+                            <li className="kg"><strong>кг</strong></li>
+                        </ul>
+                    </div>
+                    <div className="item__image">
+                        <img src="../img/cat.png" alt="кот"/>
+                    </div>
+                    
                 </div>
-                <div className="circle">
-                    <ul className="weight-list">
-                        <li className="weight"><strong>0.5</strong></li>
-                        <li className="kg"><strong>кг</strong></li>
-                    </ul>
-                </div>
-                <img src="../img/cat.jpg" alt="кот"/>
+
+                <footer className="item__footer">
+                    <span>Чего сидишь? Порадуй котэ,</span>
+                    <span><a class="item__link" href="#basket">купи/</a></span>
+                </footer>
 
             </div>
 
-            <div className="item">2</div>
-            <div className="item">3</div>
+            
+            <div className="item">
+                <div className="item__block">2</div>
+            </div>
+            <div className="item">
+                <div className="item__block">3</div>
+            </div>
         </div>
 
     </div>
