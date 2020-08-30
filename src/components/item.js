e1cf19a0ecc2add44bc810db4a7fca58d3edb006
Artyom <r.tem13@gmail.com>
2020-08-31 00:17:23 +0700

    Сделал выделение item на React

--- src/components/item.js
+++ src/components/item.js
@@ -1,31 +1,50 @@
 import React, {Component} from 'react';
 
-class Item extends Component{
-
-    state={
-
+export default class Items extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            selected: false
+        }
+        this.onSelected =this.onSelected.bind(this);
     }
 
-    handleClick = (e) => {
-        e.preventDefault();
-        console.log('Была нажата ссылка.');
-        e.currentTarget.classList.toggle("selected");
+    onSelected() {
+        this.setState(({selected})=>({
+            selected: !selected
+        }))
     }
 
+    render(){
+        /*const {selected = false} = this.props;*/
+        const {selected} = this.state;
 
-    render () {
+        let circleClasses = "circle";
+        let innerBlockClasses = "inner__block";
+        let itemParagClasses = "item__parag";
+        let triangleClasses = "corner-triangle__border";
+        let spanText = "Чего сидишь? Порадуй котэ,";
+        let buyClasses = "";
+        if (selected) {
+            circleClasses += ' selected';
+            innerBlockClasses += ' selected3';
+            itemParagClasses += ' selected2';
+            triangleClasses += ' selected4';
+            spanText = 'Печень разварная с артишоками.';
+            buyClasses = "visible";
+        }
         return (
-            <div className="item">
+            <div className="item" onClick={this.onSelected}>
 
                 <div className="item__block">
                     <div className="item__top">
                         <div className="item__corner"></div>
-                        <div className="corner-triangle__border"></div>
-                        <div className="item__parag">
+                        <div className={triangleClasses}></div>
+                        <div className={itemParagClasses}>
                             <span className="item__description">Сказочное заморское яство</span>
                         </div>
                     </div>
-                    <div className="inner__block">
+                    <div className={innerBlockClasses}>
                         <div className="item__header">
 
                             <h1>Нямушка</h1>
@@ -39,7 +58,7 @@ class Item extends Component{
                             <img src="../img/cat.png" alt="кот"/>
                         </div>
                     </div>
-                    <div className="circle target" onClick={this.handleClick}>
+                    <div className={circleClasses}>
                         <ul className="weight-list">
                             <li className="weight"><strong>0.5</strong></li>
                             <li className="kg"><strong>кг</strong></li>
@@ -50,8 +69,8 @@ class Item extends Component{
                 </div>
 
                 <footer className="item__footer">
-                    <span>Чего сидишь? Порадуй котэ,</span>
-                    <span><a class="item__link" href="#basket">купи!</a></span>
+                    <span>{spanText}</span>
+                    <span className={buyClasses}><a className="item__link" href="#basket">купи!</a></span>
                 </footer>
 
             </div>
@@ -59,4 +78,19 @@ class Item extends Component{
     }
 }
 
-export default Item;
+/*
+function Item () {
+
+    handleClick = (e) => {
+        e.preventDefault();
+        console.log('Была нажата ссылка.');
+        e.currentTarget.classList.toggle("selected");
+    }
+
+    return (
+
+    );
+
+}
+
+export default Item;*/
