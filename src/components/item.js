c4ca77639fc61d0786f40ad4a20be168924103c0
Artyom <r.tem13@gmail.com>
2020-08-30 12:42:06 +0700

    закончил CSS без выравнивания всего по центру.перехожу в JS/React

--- src/components/item.js
+++ src/components/item.js
@@ -12,14 +12,19 @@ function Item() {
                             <span className="item__description">Сказочное заморское яство</span>
                         </div>
                     </div>
-                    <div className="item__header">
-                        
-                        <h1>Нямушка</h1>
-                        <h2>с фуа-гра</h2>
-                        <ul>
-                            <li>10 порций</li>
-                            <li>1 мышь в подарок</li>
-                        </ul>
+                    <div className="inner__block">
+                        <div className="item__header">
+
+                            <h1>Нямушка</h1>
+                            <h2>с фуа-гра</h2>
+                            <ul>
+                                <li>10 порций</li>
+                                <li>1 мышь в подарок</li>
+                            </ul>
+                        </div>
+                        <div className="item__image">
+                            <img src="../img/cat.png" alt="кот"/>
+                        </div>
                     </div>
                     <div className="circle">
                         <ul className="weight-list">
@@ -27,9 +32,7 @@ function Item() {
                             <li className="kg"><strong>кг</strong></li>
                         </ul>
                     </div>
-                    <div className="item__image">
-                        <img src="../img/cat.png" alt="кот"/>
-                    </div>
+
                     
                 </div>
 
